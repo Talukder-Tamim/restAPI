57eccca147a08319b159e685cbab82ce58183be3
ubuntu <Talukder-Tamim>
2021-01-31 09:52:54 +0600

    rest api

--- language/urls.py
+++ language/urls.py
@@ -4,5 +4,7 @@ from .import views
 
 urlpatterns = [
     path('api/language', views.LanguageView.as_view()),
-    path('api/framework/', views.FrameworkView.as_view())
+    path('api/framework/', views.FrameworkView.as_view()),
+    path('api/<int:pk>', views.FrameworkAPIDetailView.as_view()),
+    path('api/new/', views.FrameworkNewView.as_view())
 ]
--- language/views.py
+++ language/views.py
@@ -1,14 +1,26 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Language, Framework
 from .serializers import LanguageAPIView, FrameworkAPIView
 
 
 class LanguageView(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Language.objects.all()
     serializer_class = LanguageAPIView
 
 
 class FrameworkView(generics.ListAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
     queryset = Framework.objects.all()
+    serializer_class = FrameworkAPIView
+
+
+class FrameworkAPIDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Framework.objects.all()
+    serializer_class = FrameworkAPIView
+
+
+class FrameworkNewView(generics.ListCreateAPIView):
+    queryset = Framework.objects.all().order_by('-id')[:2]
     serializer_class = FrameworkAPIView
